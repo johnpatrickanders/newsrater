848f778ea4faf1e80a5159a9ce60113982eb42f0
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2021-12-18 15:14:29 -0500

    initialize django, models, and postgres connection

---
+++ mysite/articles/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    length = models.IntegerField()
+    source = models.CharField()
+    url = models.URLField(max_length=300)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.url
+
+class Rating(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
