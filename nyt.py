5f48824c56adf6c19810359fda45ca4c4af6af57
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2022-03-02 16:12:20 -0500

    finish nyt scrape with url and summary

--- nyt.py
+++ nyt.py
@@ -13,6 +13,19 @@ for item in soup.select('div.story-wrapper'):
     try:
         print('----------------------------------------')
         headline = item.find('h3').get_text()
-        print(headline)
+        url = item.find('a')['href']
+        summary = item.find('p')
+        if summary:
+            summary = summary.get_text()
+
+        if url.split(".com/")[1]\
+          .split("/")[0] in ("games", "puzzles",
+                              "crosswords",
+                              "subscription",
+                              "recipes",
+                              "interactive"):
+            pass
+        else:
+            print(headline, " -- ", summary, url)
     except Exception as e:
         raise e
